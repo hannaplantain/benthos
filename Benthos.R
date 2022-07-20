0273c78bf30a4d1bb5313b5336233b62e28646eb
hannaplantain <105888147+hannaplantain@users.noreply.github.com>
2022-07-20 13:39:51 +0200

    Add files via upload

--- Benthos.R
+++ Benthos.R
@@ -1,9 +1,9 @@
 setwd("C:/Users/Hanna/Desktop/Marine Masters Summer Course/Benthos")
 
 install.packages("splancs") 
-library(splancs)
 install.packages("C:/Users/Hanna/Desktop/Marine Masters Summer Course/Benthos/RandomFields_2.0.66.tar.gz", repos = NULL, type = "source")
 install.packages("C:/Users/Hanna/Desktop/Marine Masters Summer Course/Benthos/geoR_1.8-1.tar.gz", repos = NULL, type = "source")
+library(splancs)
 library(ggplot2)
 library(dplyr)
 library(ggpubr)
@@ -56,6 +56,58 @@ coordplot <-  ggplot(NULL) +
   theme_light()
 coordplot
 
+#Temporal trend multiple species (Hanna-woendsagochtend)
+a <- read.csv('capspe_allyears.csv',header=T,na.strings=c(""))
+b <- read.csv('cirrat_allyears.csv',header=T,na.strings=c(""))
+c <- read.csv('pygele_allyears.csv',header=T,na.strings=c(""))
+d <- read.csv('urospe_allyears.csv',header=T,na.strings=c(""))
+e <- read.csv('scoloplos_allyears.csv',header=T,na.strings=c(""))
+f <- read.csv('nephtys_allyears.csv',header=T,na.strings=c(""))
+#merge failed, combined file in excel
+spec <- read.csv('species_years.csv',header=T,na.strings=c(""))
+
+#data exploration
+summary(spec)
+ggdensity(spec, x = "afdm_m2_y", color = "species", fill = "species",
+          add = "mean") + 
+  xlim(0, 5)
+ggqqplot(spec, x="afdm_m2_y", color = "species", fill = "species")
+
+model <- lm(afdm_m2_y ~ species + date, data = spec)
+dotplot <- ggplot(model, aes(x = .fitted, y = .resid)) +
+  geom_point(alpha=0.3) +
+  geom_smooth(se=TRUE, size = 1, colour="#E7B800", fill="#E7B800",  
+              fullrange   =TRUE,) +
+  geom_hline(yintercept=0, size = 1.1, col="darkorchid4", linetype="dashed") +
+  theme_minimal() +
+  ylab("Residuals") + 
+  xlab("Fitted values") +
+  ggtitle("Residuals vs. fitted") +
+  theme(plot.title = element_text(face = "bold", size = 12))
+dotplot
+
+resplot <- ggplot(model, aes(x = .resid, fill = species, color = species, alpha=0.3)) +
+  geom_histogram(bins=40, alpha=0.2) +
+  geom_density(alpha=.2) +
+  theme_minimal() +
+  ylab("Counts") + 
+  xlab("Residuals") +
+  ggtitle("Residuals histogram") +
+  theme(plot.title = element_text(face = "bold", size = 12))
+
+resplot 
+#+  scale_color_manual(values=c("#00AFBB", "darkorchid4")) + scale_fill_manual(values=c("#00AFBB", "darkorchid4"))
+
+
+resplot1 <- ggplot(model, aes(x = .resid)) + 
+  geom_histogram(color = "darkorchid4", fill="white") +
+  geom_density(alpha=.2, color="#E7B800", fill="#E7B800") +
+  theme_minimal() +
+  ylab("Residuals") + 
+  xlab("Fitted values") +
+  ggtitle("Residuals histogram") +
+  theme(plot.title = element_text(face = "bold", size = 12))
+resplot1
 
 ##Wormsss----
 nephthys <- read.csv('nephtys_allyears.csv',header=T,na.strings=c(""))
